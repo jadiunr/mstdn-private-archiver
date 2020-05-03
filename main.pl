d09a8f7b8951ecdfc71bf4543cc0763261414b0d
jadiunr <jadiunr@fout.jp>
2020-05-03 20:05:21 +0900

    Add support Slack notify

--- main.pl
+++ main.pl
@@ -6,6 +6,9 @@ use Encode;
 use Furl;
 use JSON::XS;
 
+use lib 'lib';
+use Slack::WebAPI;
+
 $|=1;
 
 my $domain = $ENV{MPA_DOMAIN_NAME};
@@ -13,6 +16,10 @@ my $access_token = $ENV{MPA_ACCESS_TOKEN};
 my $account_id = $ENV{MPA_ACCOUNT_ID};
 
 my $furl = Furl->new(headers => ['Authorization' => "Bearer $access_token"]);
+my $slack = Slack::WebAPI->new(
+    token => $ENV{MPA_SLACK_TOKEN},
+    channel_id => $ENV{MPA_SLACK_CHANNEL_ID}
+);
 
 while(1) {
     my $max_id = '9223372036854775807'; #7FFFFFFFFFFFFFFF
@@ -30,8 +37,13 @@ while(1) {
             my $created_at = Time::Piece->strptime(substr($status->{created_at}, 0, -5), "%FT%T");
             my $time_diff = $current_time - $created_at;
             if($time_diff->hours > 12) {
+                $slack->notify($status);
+                my $media_attachments = $status->{media_attachments};
+                $slack->upload($media_attachments) if $media_attachments;
+
                 $furl->delete("https://${domain}/api/v1/statuses/$status->{id}");
                 print "Deleted toot ID: $status->{id}\n";
+
                 sleep 120;
             }
         }
